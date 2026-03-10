21d36ceedaa0ac7c01c0b3b2c7f923fdc21aaa29
Shaiel1211 <dlegominecrafat@gmail.com>
2026-03-10 11:23:12 -0600

    colapila

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
+import ColaCircular.ColaCircular;
+
 public class Main {
     
     public static void main(String[] args) {
 
         System.out.println("Sistema de caferteria Universitaria");
         System.out.println("Proyecto P2 de Estructura de Datos");
+        ColaCircular cafeteria = new ColaCircular(5);
+
+        cafeteria.enqueue(1);
+        cafeteria.enqueue(2);
+        cafeteria.enqueue(3);
+
+        cafeteria.mostrar();
+
+        System.out.println("Primer pedido: " + cafeteria.peek());
+
+        cafeteria.dequeue();
+
+        cafeteria.mostrar();
+
+        pila pedidos = new pila(5);
+
+        pedidos.push("Cafe Americano");
+        pedidos.push("Capuccino");
+        pedidos.push("Latte");
+
+        pedidos.mostrar();
+
+        pedidos.pop();
+
+        pedidos.peek();
     }
 }
